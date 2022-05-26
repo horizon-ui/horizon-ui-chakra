19d3ee994363c56db3e381219ff12946cf5f80e6
Simmmple <hello@simmmple.com>
2022-05-26 13:25:20 +0300

    fixed tables + calendar card

--- src/components/calendar/MiniCalendar.js
+++ src/components/calendar/MiniCalendar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import "assets/css/MiniCalendar.css";
-import { Text, Icon, useColorModeValue } from "@chakra-ui/react";
+import { Text, Icon } from "@chakra-ui/react";
 // Chakra imports
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 // Custom components
@@ -11,12 +11,8 @@ import Card from "components/card/Card.js";
 export default function MiniCalendar(props) {
   const { selectRange, ...rest } = props;
   const [value, onChange] = useState(new Date());
-  // Chakra Color Mode
-  const borderColor = useColorModeValue("transparent", "whiteAlpha.100");
   return (
     <Card
-      border='1px solid'
-      borderColor={borderColor}
       align='center'
       direction='column'
       w='100%'
