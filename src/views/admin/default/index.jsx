446c8f838434f1499bc511908e3b27af5c34f946
nhatdeptrai23 <japnh24>
2022-11-10 22:15:00 +0700

    add space

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -63,6 +63,7 @@ export default function UserReports() {
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
   return (
+    
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
         columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
