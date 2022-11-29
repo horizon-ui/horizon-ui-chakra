ea508dcdb63b2f729c51e9d1c8665daabee9c068
Simmmple <hello@simmmple.com>
2022-11-29 12:57:34 +0200

    Cyber Monday

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -1,185 +1,212 @@
 // Chakra imports
-import { Portal, Box, useDisclosure, Text, Button, Link } from '@chakra-ui/react';
-import Footer from 'components/footer/FooterAdmin.js';
+import {
+  Portal,
+  Box,
+  useDisclosure,
+  Text,
+  Button,
+  Link,
+} from "@chakra-ui/react";
+import Footer from "components/footer/FooterAdmin.js";
 // Layout components
-import Navbar from 'components/navbar/NavbarAdmin.js';
-import Sidebar from 'components/sidebar/Sidebar.js';
-import { SidebarContext } from 'contexts/SidebarContext';
-import React, { useState } from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import Announcement from 'assets/img/Announcement.png';
-import routes from 'routes.js';
+import Navbar from "components/navbar/NavbarAdmin.js";
+import Sidebar from "components/sidebar/Sidebar.js";
+import { SidebarContext } from "contexts/SidebarContext";
+import React, { useState } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import Announcement from "assets/img/Announcement.png";
+import routes from "routes.js";
 
 // Custom Chakra theme
 export default function Dashboard(props) {
-	const { ...rest } = props;
-	// states and functions
-	const [ fixed ] = useState(false);
-	const [ toggleSidebar, setToggleSidebar ] = useState(false);
-	// functions for changing the states from components
-	const getRoute = () => {
-		return window.location.pathname !== '/admin/full-screen-maps';
-	};
-	const getActiveRoute = (routes) => {
-		let activeRoute = 'Default Brand Text';
-		for (let i = 0; i < routes.length; i++) {
-			if (routes[i].collapse) {
-				let collapseActiveRoute = getActiveRoute(routes[i].items);
-				if (collapseActiveRoute !== activeRoute) {
-					return collapseActiveRoute;
-				}
-			} else if (routes[i].category) {
-				let categoryActiveRoute = getActiveRoute(routes[i].items);
-				if (categoryActiveRoute !== activeRoute) {
-					return categoryActiveRoute;
-				}
-			} else {
-				if (window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1) {
-					return routes[i].name;
-				}
-			}
-		}
-		return activeRoute;
-	};
-	const getActiveNavbar = (routes) => {
-		let activeNavbar = false;
-		for (let i = 0; i < routes.length; i++) {
-			if (routes[i].collapse) {
-				let collapseActiveNavbar = getActiveNavbar(routes[i].items);
-				if (collapseActiveNavbar !== activeNavbar) {
-					return collapseActiveNavbar;
-				}
-			} else if (routes[i].category) {
-				let categoryActiveNavbar = getActiveNavbar(routes[i].items);
-				if (categoryActiveNavbar !== activeNavbar) {
-					return categoryActiveNavbar;
-				}
-			} else {
-				if (window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1) {
-					return routes[i].secondary;
-				}
-			}
-		}
-		return activeNavbar;
-	};
-	const getActiveNavbarText = (routes) => {
-		let activeNavbar = false;
-		for (let i = 0; i < routes.length; i++) {
-			if (routes[i].collapse) {
-				let collapseActiveNavbar = getActiveNavbarText(routes[i].items);
-				if (collapseActiveNavbar !== activeNavbar) {
-					return collapseActiveNavbar;
-				}
-			} else if (routes[i].category) {
-				let categoryActiveNavbar = getActiveNavbarText(routes[i].items);
-				if (categoryActiveNavbar !== activeNavbar) {
-					return categoryActiveNavbar;
-				}
-			} else {
-				if (window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1) {
-					return routes[i].messageNavbar;
-				}
-			}
-		}
-		return activeNavbar;
-	};
-	const getRoutes = (routes) => {
-		return routes.map((prop, key) => {
-			if (prop.layout === '/admin') {
-				return <Route path={prop.layout + prop.path} component={prop.component} key={key} />;
-			}
-			if (prop.collapse) {
-				return getRoutes(prop.items);
-			}
-			if (prop.category) {
-				return getRoutes(prop.items);
-			} else {
-				return null;
-			}
-		});
-	};
-	document.documentElement.dir = 'ltr';
-	const { onOpen } = useDisclosure();
-	document.documentElement.dir = 'ltr';
-	return (
-		<Box>
-			<Box
-				display={{ sm: 'none', lg: 'flex' }}
-				zIndex='1000'
-				bgImage={`url(${Announcement})`}
-				position='fixed'
-				h='50px'
-				width='100%'
-				alignItems='center'
-				justifyContent='center'>
-				<Text size='md' fontWeight='700' color='white'>
-					{/* This is a live preview of the PRO Version - Get access to all those
+  const { ...rest } = props;
+  // states and functions
+  const [fixed] = useState(false);
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  // functions for changing the states from components
+  const getRoute = () => {
+    return window.location.pathname !== "/admin/full-screen-maps";
+  };
+  const getActiveRoute = (routes) => {
+    let activeRoute = "Default Brand Text";
+    for (let i = 0; i < routes.length; i++) {
+      if (routes[i].collapse) {
+        let collapseActiveRoute = getActiveRoute(routes[i].items);
+        if (collapseActiveRoute !== activeRoute) {
+          return collapseActiveRoute;
+        }
+      } else if (routes[i].category) {
+        let categoryActiveRoute = getActiveRoute(routes[i].items);
+        if (categoryActiveRoute !== activeRoute) {
+          return categoryActiveRoute;
+        }
+      } else {
+        if (
+          window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1
+        ) {
+          return routes[i].name;
+        }
+      }
+    }
+    return activeRoute;
+  };
+  const getActiveNavbar = (routes) => {
+    let activeNavbar = false;
+    for (let i = 0; i < routes.length; i++) {
+      if (routes[i].collapse) {
+        let collapseActiveNavbar = getActiveNavbar(routes[i].items);
+        if (collapseActiveNavbar !== activeNavbar) {
+          return collapseActiveNavbar;
+        }
+      } else if (routes[i].category) {
+        let categoryActiveNavbar = getActiveNavbar(routes[i].items);
+        if (categoryActiveNavbar !== activeNavbar) {
+          return categoryActiveNavbar;
+        }
+      } else {
+        if (
+          window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1
+        ) {
+          return routes[i].secondary;
+        }
+      }
+    }
+    return activeNavbar;
+  };
+  const getActiveNavbarText = (routes) => {
+    let activeNavbar = false;
+    for (let i = 0; i < routes.length; i++) {
+      if (routes[i].collapse) {
+        let collapseActiveNavbar = getActiveNavbarText(routes[i].items);
+        if (collapseActiveNavbar !== activeNavbar) {
+          return collapseActiveNavbar;
+        }
+      } else if (routes[i].category) {
+        let categoryActiveNavbar = getActiveNavbarText(routes[i].items);
+        if (categoryActiveNavbar !== activeNavbar) {
+          return categoryActiveNavbar;
+        }
+      } else {
+        if (
+          window.location.href.indexOf(routes[i].layout + routes[i].path) !== -1
+        ) {
+          return routes[i].messageNavbar;
+        }
+      }
+    }
+    return activeNavbar;
+  };
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.layout === "/admin") {
+        return (
+          <Route
+            path={prop.layout + prop.path}
+            component={prop.component}
+            key={key}
+          />
+        );
+      }
+      if (prop.collapse) {
+        return getRoutes(prop.items);
+      }
+      if (prop.category) {
+        return getRoutes(prop.items);
+      } else {
+        return null;
+      }
+    });
+  };
+  document.documentElement.dir = "ltr";
+  const { onOpen } = useDisclosure();
+  document.documentElement.dir = "ltr";
+  return (
+    <Box>
+      <Box
+        display={{ sm: "none", lg: "flex" }}
+        zIndex='1000'
+        bgImage={`url(${Announcement})`}
+        position='fixed'
+        h='50px'
+        width='100%'
+        alignItems='center'
+        justifyContent='center'>
+        <Text size='md' fontWeight='700' color='white'>
+          {/* This is a live preview of the PRO Version - Get access to all those
           features with Horizon PRO! */}
-					Black Friday 50% Discount to Horizon PRO | Limited offer - Lifetime access
-				</Text>
-				<Button
-					ms='10px'
-					h='30px'
-					isExtert
-					w='110px'
-					href
-					bg='whiteAlpha.300'
-					_hover={{ bg: 'whiteAlpha.400' }}
-					_active={{ bg: 'whiteAlpha.400' }}
-					color='white'>
-					<Link isExternal='true' href='https://horizon-ui.com/pro?ref=bf-live-preview-pro-announcement'>
-						Grab Offer
-					</Link>
-				</Button>
-			</Box>
-			<Box pt={{ sm: '0px', lg: '50px' }}>
-				<SidebarContext.Provider
-					value={{
-						toggleSidebar,
-						setToggleSidebar
-					}}>
-					<Sidebar routes={routes} display='none' {...rest} />
-					<Box
-						float='right'
-						minHeight='100vh'
-						height='100%'
-						overflow='auto'
-						position='relative'
-						maxHeight='100%'
-						w={{ base: '100%', xl: 'calc( 100% - 290px )' }}
-						maxWidth={{ base: '100%', xl: 'calc( 100% - 290px )' }}
-						transition='all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)'
-						transitionDuration='.2s, .2s, .35s'
-						transitionProperty='top, bottom, width'
-						transitionTimingFunction='linear, linear, ease'>
-						<Portal>
-							<Box>
-								<Navbar
-									onOpen={onOpen}
-									logoText={'Horizon UI Dashboard PRO'}
-									brandText={getActiveRoute(routes)}
-									secondary={getActiveNavbar(routes)}
-									message={getActiveNavbarText(routes)}
-									fixed={fixed}
-									{...rest}
-								/>
-							</Box>
-						</Portal>
+          Cyber Monday 50% Discount to Horizon PRO | Limited offer - Lifetime
+          access
+        </Text>
+        <Button
+          ms='10px'
+          h='30px'
+          isExtert
+          w='110px'
+          href
+          bg='whiteAlpha.300'
+          _hover={{ bg: "whiteAlpha.400" }}
+          _active={{ bg: "whiteAlpha.400" }}
+          color='white'>
+          <Link
+            isExternal='true'
+            href='https://horizon-ui.com/pro?ref=bf-live-preview-pro-announcement'>
+            Grab Offer
+          </Link>
+        </Button>
+      </Box>
+      <Box pt={{ sm: "0px", lg: "50px" }}>
+        <SidebarContext.Provider
+          value={{
+            toggleSidebar,
+            setToggleSidebar,
+          }}>
+          <Sidebar routes={routes} display='none' {...rest} />
+          <Box
+            float='right'
+            minHeight='100vh'
+            height='100%'
+            overflow='auto'
+            position='relative'
+            maxHeight='100%'
+            w={{ base: "100%", xl: "calc( 100% - 290px )" }}
+            maxWidth={{ base: "100%", xl: "calc( 100% - 290px )" }}
+            transition='all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)'
+            transitionDuration='.2s, .2s, .35s'
+            transitionProperty='top, bottom, width'
+            transitionTimingFunction='linear, linear, ease'>
+            <Portal>
+              <Box>
+                <Navbar
+                  onOpen={onOpen}
+                  logoText={"Horizon UI Dashboard PRO"}
+                  brandText={getActiveRoute(routes)}
+                  secondary={getActiveNavbar(routes)}
+                  message={getActiveNavbarText(routes)}
+                  fixed={fixed}
+                  {...rest}
+                />
+              </Box>
+            </Portal>
 
-						{getRoute() ? (
-							<Box mx='auto' p={{ base: '20px', md: '30px' }} pe='20px' minH='100vh' pt='50px'>
-								<Switch>
-									{getRoutes(routes)}
-									<Redirect from='/' to='/admin/default' />
-								</Switch>
-							</Box>
-						) : null}
-						<Box>
-							<Footer />
-						</Box>
-					</Box>
-				</SidebarContext.Provider>
-			</Box>
-		</Box>
-	);
+            {getRoute() ? (
+              <Box
+                mx='auto'
+                p={{ base: "20px", md: "30px" }}
+                pe='20px'
+                minH='100vh'
+                pt='50px'>
+                <Switch>
+                  {getRoutes(routes)}
+                  <Redirect from='/' to='/admin/default' />
+                </Switch>
+              </Box>
+            ) : null}
+            <Box>
+              <Footer />
+            </Box>
+          </Box>
+        </SidebarContext.Provider>
+      </Box>
+    </Box>
+  );
 }
