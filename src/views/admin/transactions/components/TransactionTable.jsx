91cc6dbe3a2bdbde841e7df6fb54294ee221aec6
qhuong1008 <qhuong1008@gmail.com>
2024-04-28 20:16:39 +0700

    update transaction ui

--- src/views/admin/transactions/components/TransactionTable.jsx
+++ src/views/admin/transactions/components/TransactionTable.jsx
@@ -1,15 +1,136 @@
-import React from 'react'
+import {
+  Button,
+  Flex,
+  Link,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+  useColorModeValue
+} from "@chakra-ui/react";
 import Card from "components/card/Card";
+import React, { useState } from "react";
+import { Icon } from "@chakra-ui/react";
+import {
+  MdEdit,
+  MdRemoveCircle,
+  MdAdd,
+} from "react-icons/md";
+import { SearchBar } from "components/navbar/searchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
 
 const TransactionTable = () => {
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
+
   return (
     <Card
       direction="column"
       w="100%"
-      px="20px"
-
+      px="0px"
+      overflowX={{ sm: "scroll" }}
+      overflowY={{ sm: "scroll" }}
     >
-      TransactionTable
+      <SearchBar mx="20px" mb="10px" />
+
+      <Flex px="25px" justify="space-between" mb="20px" align="center">
+        <Text
+          color={textColor}
+          fontSize="22px"
+          fontWeight="700"
+          lineHeight="100%"
+        >
+          Tag Manage
+        </Text>
+        <Link href="/admin/tag/new">
+          <Button>
+            <Icon
+              as={MdAdd}
+              width="20px"
+              height="20px"
+              color="inherit"
+              cursor="pointer"
+            />
+            Add new tag
+          </Button>
+        </Link>
+      </Flex>
+      <Table variant="simple" color="gray.500" mb="24px">
+        <Thead>
+
+          <Tr>
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >ID</Flex>
+            </Th>
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >Người dùng</Flex>
+            </Th>
+
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >Loại thanh toán</Flex>
+            </Th>
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >Sản phẩm</Flex>
+            </Th>
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >Thời gian</Flex>
+            </Th>
+            <Th pe="10px" borderColor={borderColor}>
+              <Flex
+                justify="space-between"
+                align="center"
+                fontSize={{ sm: "10px", lg: "12px" }}
+                color="gray.400"
+              >Tổng tiền</Flex>
+            </Th>
+          </Tr>
+
+        </Thead>
+
+        <Tbody>
+          <Tr>
+            <Td>name</Td>
+            <Td>desc</Td>
+            <Td>desc</Td>
+            <Td>desc</Td>
+            <Td>desc</Td>
+            <Td>desc</Td>
+
+          </Tr>
+
+        </Tbody>
+      </Table>
+
+
+      <Toaster />
     </Card>
   )
 }
--- src/views/admin/transactions/index.jsx
+++ src/views/admin/transactions/index.jsx
@@ -1,12 +1,49 @@
 
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Input, Text, useColorModeValue } from "@chakra-ui/react";
 import React from "react";
 import TransactionTable from "./components/TransactionTable";
 import InfoTransaction from "./components/InfoTransaction";
+import Card from "components/card/Card";
 
 export default function Transactions() {
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
+      <Card
+        direction="column"
+        w="100%"
+        px="20px"
+        my="10px"
+      >
+        <Text
+          color={textColor}
+          fontSize="22px"
+          fontWeight="700"
+          lineHeight="100%"
+        >
+          Doanh thu tính từ:
+        </Text>
+        <Flex
+          my="15px"
+          flexDirection="row"
+          alignItems={"center"}
+        >
+          <Input placeholder='Select Date and Time' size='md' type='month' />
+          <Text
+            color={textColor}
+            fontSize="17px"
+            fontWeight="500"
+            lineHeight="100%"
+            margin={"0 30px"}
+          >
+            đến:
+          </Text>
+          <Input placeholder='Select Date and Time' size='md' type='month' />
+
+        </Flex>
+
+      </Card>
       <InfoTransaction />
       <TransactionTable />
     </Box>
