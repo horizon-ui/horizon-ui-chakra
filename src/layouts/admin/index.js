57b12285799c7cb69f4f5bc71f932a9ae9962761
qhuong1008 <qhuong1008@gmail.com>
2024-06-14 20:29:27 +0700

    Update index.js

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -16,18 +16,20 @@ import { SidebarContext } from "contexts/SidebarContext";
 import React, { useEffect, useState } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import routes from "routes.js";
+import { useHistory } from "react-router-dom";
 
 // Custom Chakra theme
 export default function Dashboard(props) {
   // const isAuthenticated = JSON.parse(localStorage.getItem("authenticated"));
   // const isAuthenticated = true;
+  const history = useHistory();
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem("user"));
     if (user) {
-      if (user.role === 1) {
-        setIsAuthenticated(true);
+      if (user.role !== 1) {
+        history.push("/login");
       }
     }
   }, []);
@@ -136,69 +138,63 @@ export default function Dashboard(props) {
   const { onOpen } = useDisclosure();
   document.documentElement.dir = "ltr";
   return (
-    <>
-      {isAuthenticated ? (
-        <Box>
-          <Box>
-            <SidebarContext.Provider
-              value={{
-                toggleSidebar,
-                setToggleSidebar,
-              }}
-            >
-              <Sidebar routes={routes} display="none" {...rest} />
-              <Box
-                float="right"
-                minHeight="100vh"
-                height="100%"
-                overflow="auto"
-                position="relative"
-                maxHeight="100%"
-                w={{ base: "100%", xl: "calc( 100% - 290px )" }}
-                maxWidth={{ base: "100%", xl: "calc( 100% - 290px )" }}
-                transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
-                transitionDuration=".2s, .2s, .35s"
-                transitionProperty="top, bottom, width"
-                transitionTimingFunction="linear, linear, ease"
-              >
-                <Portal>
-                  <Box>
-                    <Navbar
-                      onOpen={onOpen}
-                      logoText={"Horizon UI Dashboard PRO"}
-                      brandText={getActiveRoute(routes)}
-                      secondary={getActiveNavbar(routes)}
-                      message={getActiveNavbarText(routes)}
-                      fixed={fixed}
-                      {...rest}
-                    />
-                  </Box>
-                </Portal>
-
-                {getRoute() ? (
-                  <Box
-                    mx="auto"
-                    p={{ base: "20px", md: "30px" }}
-                    pe="20px"
-                    minH="100vh"
-                    pt="50px"
-                  >
-                    <Switch>
-                      {getRoutes(routes)}
-                      <Redirect from="/" to="/admin" />
-                    </Switch>
-                  </Box>
-                ) : null}
-                <Box>
-                  <Footer />
-                </Box>
+    <Box>
+      <Box>
+        <SidebarContext.Provider
+          value={{
+            toggleSidebar,
+            setToggleSidebar,
+          }}
+        >
+          <Sidebar routes={routes} display="none" {...rest} />
+          <Box
+            float="right"
+            minHeight="100vh"
+            height="100%"
+            overflow="auto"
+            position="relative"
+            maxHeight="100%"
+            w={{ base: "100%", xl: "calc( 100% - 290px )" }}
+            maxWidth={{ base: "100%", xl: "calc( 100% - 290px )" }}
+            transition="all 0.33s cubic-bezier(0.685, 0.0473, 0.346, 1)"
+            transitionDuration=".2s, .2s, .35s"
+            transitionProperty="top, bottom, width"
+            transitionTimingFunction="linear, linear, ease"
+          >
+            <Portal>
+              <Box>
+                <Navbar
+                  onOpen={onOpen}
+                  logoText={"Horizon UI Dashboard PRO"}
+                  brandText={getActiveRoute(routes)}
+                  secondary={getActiveNavbar(routes)}
+                  message={getActiveNavbarText(routes)}
+                  fixed={fixed}
+                  {...rest}
+                />
               </Box>
-            </SidebarContext.Provider>
+            </Portal>
+
+            {getRoute() ? (
+              <Box
+                mx="auto"
+                p={{ base: "20px", md: "30px" }}
+                pe="20px"
+                minH="100vh"
+                pt="50px"
+              >
+                <Switch>
+                  {getRoutes(routes)}
+                  <Redirect from="/" to="/admin" />
+                </Switch>
+              </Box>
+            ) : null}
+            <Box>
+              <Footer />
+            </Box>
           </Box>
-        </Box>
-      ) : (
-        <></>
-      )}
-    </>
+        </SidebarContext.Provider>
+      </Box>
+    </Box>
   );
 }
